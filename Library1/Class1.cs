549c5da3f8c57a432e2fe587edacdeabf56e7dd0
il226 <il226@HUAWEI>
2025-03-31 10:31:55 +0300

    bugfix3

---
+++ Library1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Library1
+{
+    public class MathOperations
+    {
+        // Метод для сложения
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Метод для вычитания
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Метод для умножения
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Метод для деления
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно!");
+            return (double)a / b;
+        }
+
+    }
+
+    public class Library
+    {
+        static void Main()
+        {
+            var math = new MathOperations();
+            Console.WriteLine($"5 + 10 = {math.Add(5, 10)}");
+
+            MathOperations obj = new MathOperations();
+
+        }
+    }
+}
+
