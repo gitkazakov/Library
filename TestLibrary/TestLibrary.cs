786d768a14cc99a74cb5e9c6ac514a8afcf12cc2
il226 <il226@HUAWEI>
2025-03-31 10:06:06 +0300

    Добавьте файлы проекта.

---
+++ TestLibrary/TestLibrary.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using Library;
+using System;
+
+[TestFixture]
+public class MathOperationsTests
+{
+    private readonly MathOperations _mathOperations;
+
+    public MathOperationsTests()
+    {
+        _mathOperations = new MathOperations();
+    }
+
+    [Test]
+    public void Add_ShouldReturnCorrectResult()
+    {
+        int result = _mathOperations.Add(5, 10);
+        Assert.AreEqual(15, result);
+    }
+
+    [Test]
+    public void Subtract_ShouldReturnCorrectResult()
+    {
+        int result = _mathOperations.Subtract(10, 5);
+        Assert.AreEqual(5, result);
+    }
+
+    [Test]
+    public void Multiply_ShouldReturnCorrectResult()
+    {
+        int result = _mathOperations.Multiply(5, 4);
+        Assert.AreEqual(20, result);
+    }
+
+    [Test]
+    public void Divide_ShouldReturnCorrectResult()
+    {
+        double result = _mathOperations.Divide(10, 2);
+        Assert.AreEqual(5.0, result);
+    }
+
+    [Test]
+    public void Divide_ByZero_ShouldThrowException()
+    {
+        Assert.Throws<DivideByZeroException>(() => _mathOperations.Divide(10, 0));
+    }
+}
